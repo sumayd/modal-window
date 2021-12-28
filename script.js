8da85ce90bb66e7d131ef954900bcdd9794cdfd7
sumayd <sumeyyeaydemir@gmail.com>
2021-12-28 18:19:30 +0300

    modal done

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const openModelBtn = document.querySelector('.show-modal')
+const closeModalBtn = document.querySelector('.close-modal')
+
+
+const openModel = function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+openModelBtn.addEventListener('click', openModel)
+closeModalBtn.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Escape'){
+        if(!modal.classList.contains('hidden')){
+            closeModal()
+        }
+    }
+})
