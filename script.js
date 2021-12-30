7b1508f9c54e6333d1d64d7e771c55e4133f75c3
sumayd <sumeyyeaydemir@gmail.com>
2021-12-30 11:57:07 +0300

    update js file

--- script.js
+++ script.js
@@ -21,9 +21,7 @@ closeModalBtn.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
 
 document.addEventListener('keydown', function(e) {
-    if(e.key === 'Escape'){
-        if(!modal.classList.contains('hidden')){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
             closeModal()
-        }
     }
 })
